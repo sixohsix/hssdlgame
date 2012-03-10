33028638cfe7dfa34fc5c7e226cd967b91ab73ec
Mike Verdone <mike.verdone@gmail.com>
2012-03-10 19:04:07 +0100

    Now with a cabal file.

--- HsSdlTest.hs
+++ HsSdlTest.hs
@@ -10,11 +10,15 @@ background = "background.bmp"
 
 my_main = SDL.withInit [InitEverything] $ do
   SDL.setVideoMode 640 480 24 [SDL.SWSurface]
+  SDL.setCaption "hello world!" "hello world"
   back <- SDL.loadBMP background
   screen <- SDL.getVideoSurface
   blitSurface back Nothing screen Nothing
   SDL.flip screen
-  SDL.delay 2000
+  eventLoop
+  where eventLoop = SDL.waitEventBlocking >>= checkEvent
+        checkEvent (KeyUp _) = return ()
+        checkEvent _ = eventLoop
 
 
 foreign export ccall my_main :: IO ()
